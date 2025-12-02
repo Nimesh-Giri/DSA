d5107645625198cc8571ae35776bafb2444539fb
Nimesh-Giri <nimeshgiri871@gmail.com>
2025-12-02 20:30:46 +0545

    completed code of multidimensional array

--- Array/multidimensional_array.cpp
+++ Array/multidimensional_array.cpp
@@ -4,3 +4,52 @@ multi dimensional array is equals to the product of its boundaries.*/
 
 //WAP to input a 3 x 3 matrix and find out the sum of lower triangular elements.
 
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int mat[3][3],i,j,sum=0;
+
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cout<<"Enter the element :";
+            cin>>mat[i][j];
+
+        }
+    }
+
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+           if(i>=j)
+           {
+            sum= sum+mat[i][j];
+           }
+
+        }
+    }
+
+
+     cout<<"\nThe matrix is\n";
+    for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            
+            cout<<"\t"<<mat[i][j];
+            
+
+        }
+        cout<<"\n";
+        
+    }
+    cout<<"The sum of elements of the lower triangle is:"<<sum;
+
+    
+}
+
